df742837159d7e97684d43d4c1e8771cdfd33751
Damian <damian.nykiel.programista@gmail.com>
2020-07-05 18:40:44 +0200

    All files clear

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
 
+#include "PersonalBudget.h"
+
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+
+    //PersonalBudget personalBudget;
+
+    char wybor;
+
+    while (true) {
+        if (!personalBudget.czyUzytkownikJestZalogowany()) {
+            wybor = personalBudget.wybierzOpcjeZMenuGlownego();
+
+            switch (wybor) {
+            case '1':
+                personalBudget.rejestracjaUzytkownika();
+                break;
+            case '2':
+                personalBudget.logowanieUzytkownika();
+                break;
+            case '9':
+                exit(0);
+                break;
+            default:
+                cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
+                system("pause");
+                break;
+            }
+        } else {
+            wybor = personalBudget.wybierzOpcjeZMenuUzytkownika();
+
+            switch (wybor) {
+            case '1':
+                //personalBudget.dodajAdresata();
+                break;
+            case '2':
+                //personalBudget.wyszukajAdresatowPoImieniu();
+                break;
+            case '3':
+                //personalBudget.wyszukajAdresatowPoNazwisku();
+                break;
+            case '4':
+                //personalBudget.wyswietlWszystkichAdresatow();
+                break;
+            case '5':
+                //personalBudget.usunAdresata();
+                break;
+            case '6':
+                //personalBudget.edytujAdresata();
+                break;
+            case '7':
+                //personalBudget.zmianaHaslaZalogowanegoUzytkownika();
+                break;
+            case '8':
+                //personalBudget.wylogowanieUzytkownika();
+                break;
+            }
+        }
+    }
     return 0;
 }
